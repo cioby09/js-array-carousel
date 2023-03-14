7984b7aa038723c36d881b3cd1f0b67dbf4d88a7
cioby09 <claudiociobanu09@gmail.com>
2023-03-14 20:37:16 +0100

    Milestone 3 and Bonus 1

--- js/script.js
+++ js/script.js
@@ -23,3 +23,77 @@ const itemsArray = document.getElementsByClassName("item");
 //Fisso la prima immagine
 let activeItemIndex = 0;
 itemsArray[activeItemIndex].classList.add("active");
+
+// Gestisco il click sul bottone "down"
+const downBtn = document.querySelector(".down");
+
+downBtn.addEventListener("click", 
+
+    function() {
+
+        if (activeItemIndex < (itemsArray.length - 1)) {
+
+            // Tolgo "active" dalla slide corrente
+            itemsArray[activeItemIndex].classList.remove("active");
+
+            // Incremento il valore di "activeIndex"
+            activeItemIndex++;
+
+            // Aggiungo "active" alla prossima slide
+            itemsArray[activeItemIndex].classList.add("active");
+
+          // Una volta arrivato all'ultima slide rendo lo scorrimento continuo  
+          // Se sono all'ultima slide  
+        } else if (activeItemIndex === itemsArray.length - 1) {
+
+             // Tolgo "active" dalla slide corrente
+             itemsArray[activeItemIndex].classList.remove("active");
+
+             // Il valore di "activeIndex" torna a quello della prima slide
+             activeItemIndex = 0;
+ 
+             // Aggiungo "active" alla prossima slide
+             itemsArray[activeItemIndex].classList.add("active");
+
+        }
+
+    }
+
+);
+
+// Gestisco il click sul bottone "down"
+const upBtn = document.querySelector(".up");
+
+upBtn.addEventListener("click", 
+
+    function() {
+
+          // Se mi trovo alla prima slide rendo lo scorrimento continuo  
+          // Se sono alla prima slide
+         if (activeItemIndex === 0) {
+
+            // Tolgo "active" dalla slide corrente
+            itemsArray[activeItemIndex].classList.remove("active");
+
+            // Il valore di "activeIndex" torna a quello dell'ultima slide
+            activeItemIndex = itemsArray.length - 1;
+
+            // Aggiungo "active" alla slide precedente
+            itemsArray[activeItemIndex].classList.add("active");
+
+         } else if (activeItemIndex < (itemsArray.length)) {
+
+            // Tolgo "active" dalla slide corrente
+            itemsArray[activeItemIndex].classList.remove("active");
+
+            // Decremento il valore di "activeIndex"
+            activeItemIndex--;
+
+            // Aggiungo "active" alla slide precedente
+            itemsArray[activeItemIndex].classList.add("active");
+
+         }
+
+    }
+
+)
